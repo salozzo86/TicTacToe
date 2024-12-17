3af28f61b81aeb399f45724d22b58e223facef2f
salozzo86 <antonio.malafronte86@gmail.com>
2024-12-17 21:50:14 +0100

    Initial Commit

---
+++ TicTacToe/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  TicTacToe
+//
+//  Created by Antonio Malafronte on 2024/12/17.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
